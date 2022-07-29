a87121725469c693c63e04c95ba5aaefbeb8e188
mrsherobrine <mrsherobrinenaomi@gmail.com>
2022-07-29 11:59:14 +0200

    works

---
+++ Smithle.cs
@@ -0,0 +1,21 @@
+﻿using Raylib_cs;
+
+class Smithle
+{
+	static void Main(String[] args)
+	{
+		Raylib.InitWindow(600, 480, "Smithle");
+
+		while (!Raylib.WindowShouldClose())
+		{
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(Color.WHITE);
+
+			Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+			Raylib.EndDrawing();
+		}
+
+		Raylib.CloseWindow();
+	}
+}
